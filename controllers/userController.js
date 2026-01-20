ee4cd8e207776b0d536a3955a7e392e48122302d
Sarvjot <sarvjotmalhotra@gmail.com>
2026-01-20 13:05:29 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-const { generateToken, jwtAuthMiddleware } = require("../middleware/jwt.js");
+const { generateToken, jwtAuthMiddleware } = require("../jwt.js");
 
 exports.signup = async (req, res) => {
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const user = require("../models/userModel");
 
 const userController = require("../controllers/userController");
-const { generateToken, jwtAuthMiddleware } = require("../middleware/jwt.js");
+const { generateToken, jwtAuthMiddleware } = require("../jwt.js");
 
 router.post("/signup", userController.signup);
 //login route
